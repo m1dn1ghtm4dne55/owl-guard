a90ebc1574a50af41bcca24bd2cf8784d409b7fb
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-11-29 03:25:14 +0300

    построена черновая логика работы

--- ssh_owl/main.py
+++ ssh_owl/main.py
@@ -1,15 +1,37 @@
 from os import getenv
+from time import sleep
+from datetime import datetime
 
-from notification_util import MessageSender
+
 from dotenv import load_dotenv
+
+from sessions_looker import SessionsLooker
+from notification_util import MessageSender
 
 load_dotenv()
 
 TOKEN = getenv('TOKEN')
 USER_ID = getenv('USER_ID')
+timeout = 1
 
-manager = MessageSender(TOKEN, USER_ID)
-manager.send_message_to_user('Я из бубунты')
+http_manager = MessageSender(TOKEN, USER_ID)
+session_manager = SessionsLooker()
 
 if __name__ == '__main__':
-    pass
+    while True:
+        sleep(timeout)
+        ssh_users = session_manager.get_login_users()
+        if session_manager.get_ssh_user_count == 0:
+            session_manager.set_ssh_user_count(len(ssh_users))
+            http_manager.send_message_to_user('SSH Owl запущена, текущие подключения:')
+            for user in ssh_users:
+                http_manager.send_message_to_user(user)
+        else:
+            if session_manager.get_ssh_user_count != len(ssh_users):
+                count_difference = session_manager.get_ssh_user_count - len(ssh_users)
+                session_manager.set_ssh_user_count(len(ssh_users))
+                last_user = session_manager.get_ssh_user_old()[-1]
+                if count_difference > 0:
+                    http_manager.send_message_to_user(f'{datetime.now()} | Отключился пользователь {last_user}\n')
+                else:
+                    http_manager.send_message_to_user(f'{datetime.now()} | Отключился пользователь {last_user}\n')
