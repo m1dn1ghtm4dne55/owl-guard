eb2bc73571d7072dd95a8cb3bf40f2f7cdc9695e
artemp <d_o_b@bk.ru>
2025-11-30 22:57:16 +0200

    асинхронный пост для телеграмм

---
+++ ssh_owl/async_notification_util.py
@@ -0,0 +1,23 @@
+import json
+from os import getenv
+import asyncio
+
+from dotenv import load_dotenv
+from aiohttp import ClientSession, TCPConnector
+
+load_dotenv()
+
+TOKEN = getenv('TOKEN')
+USER_ID = getenv('USER_ID')
+
+
+class AsyncMessageSender:
+    def __init__(self, token: str, user_id: str):
+        self._user_id = user_id
+        self._url = f'https://api.telegram.org/bot{token}/SendMessage'
+
+    async def send_message_to_user(self, message):
+        connector = TCPConnector(ssl=False)
+        body = {"chat_id": self._user_id, "text": message}
+        async with ClientSession(connector=connector) as session:
+            response = await session.post(url=self._url, data=body)
