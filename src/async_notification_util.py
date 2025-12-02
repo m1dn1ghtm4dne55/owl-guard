3585af269033f34e50c7b4d6b4340af88b42675b
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-02 19:24:40 +0300

    добавлен проброс ошибок, изменена ссылка на ту что в документации API TG, хотя и так работало.

--- src/async_notification_util.py
+++ src/async_notification_util.py
@@ -12,10 +12,13 @@ USER_ID = getenv('USER_ID')
 class AsyncMessageSender:
     def __init__(self, token: str, user_id: str):
         self._user_id = user_id
-        self._url = f'https://api.telegram.org/bot{token}/SendMessage'
+        self._url = f'https://api.telegram.org/bot{token}/sendMessage'
 
     async def send_message_to_user(self, message):
         connector = TCPConnector(ssl=False)
         body = {"chat_id": self._user_id, "text": message}
         async with ClientSession(connector=connector) as session:
             response = await session.post(url=self._url, data=body)
+            response.raise_for_status()
+            data = await response.json()
+            return data
