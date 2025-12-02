fa77417c7d1ff8307991df41a06ab7bf0470bb84
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-02 04:15:58 +0300

    утилита преобразующая вывод в человекочитаемый

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+def human_read_response(payload: dict):
+    lines = []
+    for key, value in payload.items():
+        if key == 'Timestamp':
+            dt = datetime.fromtimestamp(value / 1000000)
+            lines.append(f'{key}: {dt.strftime("%Y-%m-%d %H:%M:%S")}')
+        else:
+            lines.append(f'{key}: {value}')
+    result = '\n'.join(lines)
+    return f'New login\n\n{result}'
