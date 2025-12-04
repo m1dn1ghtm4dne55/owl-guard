cee058f7dbd4a5784b92d7025e41ddf1e7a2d18b
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-04 17:43:37 +0300

    убраны лишние импорты и переменные

--- src/notification_util.py
+++ src/notification_util.py
@@ -1,24 +1,16 @@
-from os import getenv
-
-from dotenv import load_dotenv
 from aiohttp import ClientSession, TCPConnector
 
-load_dotenv()
-
-TOKEN = getenv('TOKEN')
-USER_ID = getenv('USER_ID')
-
-
 class AsyncMessageSender:
-    def __init__(self, token: str, user_id: str):
+    def __init__(self, token: str, user_id: str, url:str = 'https://api.telegram.org/bot'):
         self._user_id = user_id
-        self._url = f'https://api.telegram.org/bot{token}/sendMessage'
+        self._url = f'{url}'
+        self._token = token
 
     async def send_message_to_user(self, message):
         connector = TCPConnector(ssl=False)
         body = {"chat_id": self._user_id, "text": message}
         async with ClientSession(connector=connector) as session:
-            response = await session.post(url=self._url, data=body)
+            response = await session.post(url=f'{self._url}{self._token}/sendMessage', data=body)
             response.raise_for_status()
             data = await response.json()
             return data
