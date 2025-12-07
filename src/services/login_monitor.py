ef1c596a039b68cf16909d9397c90cec466413bb
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-08 00:36:33 +0300

    кеширование сессий, убрана отправка всех сессий при инициализации, метод будет переработан

--- src/services/login_monitor.py
+++ src/services/login_monitor.py
@@ -3,6 +3,7 @@ from dbus_next import DBusError
 from core.dbus import DBusConnector, LoginSessionService
 from services.notification_service import NotificationService
 from utils.logger.log_manager import get_logger
+from services.cache_session_service import SessionCacheService
 
 
 class LoginMonitor:
@@ -11,18 +12,26 @@ class LoginMonitor:
         self._logger = get_logger('dev')
         self._session = session_service
         self._notify = notify_service
+        self._cache = SessionCacheService()
+
 
     async def _on_session_new(self, session_id: str, path: str) -> None:
         payload = await self._session.get_session_property(session_id, path)
+        self._cache.session_add(session_id, payload)
         await self._notify.session_new(payload)
 
     async def _on_session_removed(self, session_id: str, path: str) -> None:
-        await self._notify.session_removed(session_id, path)
+        payload = self._cache.session_get(session_id)
+        await self._notify.session_terminate(payload)
+        self._cache.session_remove(session_id)
+        self._logger.info(self._cache.all_session_get_info())
 
     async def _sessions_info(self, sessions: list):
         for sess in sessions:
             payload = await self._session.get_session_property(session_id=sess[0], path=sess[-1])
-            await self._notify.all_active_session(payload)
+            self._cache.session_add(sess[0], payload)
+        all_session = self._cache.all_session_get_info()
+        self._logger.info(all_session)
 
     async def run_monitoring(self):
         try:
