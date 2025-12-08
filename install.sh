d37b8d011d791cf13692c346f930a55653467a96
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-09 01:37:22 +0300

    Начало инсталяционного скрипта

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+REPO_URL=https://github.com/m1dn1ghtm4dne55/owl-guard/archive/refs/heads/master.zip
+SERVICE_PATH=/opt/owl-guard/
+ENV_PATH="$SERVICE_PATH"/src/.env
+
+if [ "$(whoami)" != "root" ]; then
+        echo "Please start me from root"
+        exit
+fi
+
+wget -q "$REPO_URL" -O /tmp/logger.zip
+unzip -q -o /tmp/logger.zip -d  "$SERVICE_PATH"
+rm /tmp/logger.zip
+touch /opt/owl-guard-service/.env
+
+
+read -rp "TELEGRAM_BOT_TOKEN: " TELEGRAM_BOT_TOKEN || true
+if [ -n "${TELEGRAM_BOT_TOKEN}" ]; then
+        echo "TELEGRAM_BOT_TOKEN='${TELEGRAM_BOT_TOKEN}'" >> "$ENV_PATH"
+fi
+read -rp "TELEGRAM_USER_ID: " TELEGRAM_USER_ID || true
+if [ -n "${TELEGRAM_USER_ID}" ]; then
+        echo "TELEGRAM_USER_ID='${TELEGRAM_USER_ID}'" >> "$ENV_PATH"
+fi
