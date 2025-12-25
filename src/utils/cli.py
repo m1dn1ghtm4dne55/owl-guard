e43663c9d7e0cb3cf829c02612eab68882a82975
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-25 23:48:35 +0300

    обновлены названия функций

--- src/utils/cli.py
+++ src/utils/cli.py
@@ -3,12 +3,12 @@ import argparse
 parser = argparse.ArgumentParser()
 
 
-def get_filename(args: argparse.Namespace):
+def get_telegram_token(args: argparse.Namespace):
     with open('.env', 'r') as file:
         print(file.read())
 
 
-def set_token(args: argparse.Namespace):
+def set_telegram_token(args: argparse.Namespace):
     with open('.env', 'w') as w_file:
         w_file.write(f"TELEGRAM_BOT_TOKEN='{args.token}'")
 
@@ -16,10 +16,10 @@ def set_token(args: argparse.Namespace):
 def cli():
     subparser = parser.add_subparsers(required=True)
     p_get = subparser.add_parser("get_token", help="Get info Telegram bot token")
-    p_get.set_defaults(func=get_filename)
+    p_get.set_defaults(func=get_telegram_token)
     p_set = subparser.add_parser("set_token", help="Set Telegram bot token")
     p_set.add_argument("token", help="Telegram token")
-    p_set.set_defaults(func=set_token)
+    p_set.set_defaults(func=set_telegram_token)
     args = parser.parse_args()
     args.func(args)
 
