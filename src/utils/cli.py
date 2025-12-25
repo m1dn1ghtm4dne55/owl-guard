44e22886c0073be20f5e995ec9fb2cc7b5084f29
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-25 23:42:30 +0300

    Черновая версия, редактирование токена телеграм

---
+++ src/utils/cli.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+def get_filename(args: argparse.Namespace):
+    with open('.env', 'r') as file:
+        print(file.read())
+
+
+def set_token(args: argparse.Namespace):
+    with open('.env', 'w') as w_file:
+        w_file.write(f"TELEGRAM_BOT_TOKEN='{args.token}'")
+
+
+def cli():
+    subparser = parser.add_subparsers(required=True)
+    p_get = subparser.add_parser("get_token", help="Get info Telegram bot token")
+    p_get.set_defaults(func=get_filename)
+    p_set = subparser.add_parser("set_token", help="Set Telegram bot token")
+    p_set.add_argument("token", help="Telegram token")
+    p_set.set_defaults(func=set_token)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    cli()
