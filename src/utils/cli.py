1755196ed453249bfc90cf0381ca84f24a40f4ec
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-28 21:56:52 +0300

    черновик, пока в функциональном

--- src/utils/cli.py
+++ src/utils/cli.py
@@ -1,28 +1,64 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-
-def get_telegram_token(args: argparse.Namespace):
-    with open('.env', 'r') as file:
-        print(file.read())
+from typing import Optional
+from argparse import Namespace, ArgumentParser
 
 
-def set_telegram_token(args: argparse.Namespace):
-    with open('.env', 'w') as w_file:
-        w_file.write(f"TELEGRAM_BOT_TOKEN='{args.token}'")
+def read_env(env_path: str) -> list[str]:
+    with open(env_path, 'r') as file:
+        lines = file.readlines()
+        return lines
+
+
+def write_env(new_data: list[str], env_path: str):
+    with open(env_path, 'w') as file:
+        file.writelines(new_data)
+
+
+def get_env_value(key: str, env_path: str) -> Optional[str]:
+    lines = read_env(env_path)
+    for line in lines:
+        if line.startswith(key):
+            value = line.split('=')[-1].strip()
+            if value[0] and value[-1] == "'":
+                value = value[1:-1]
+                return value
+            return value
+    return None
+
+
+def set_env_value(key: str, line: str, env_path: str) -> list[str]:
+    lines = read_env(env_path)
+    new_line = f"{key}='{line}'\n"
+    for i, line in enumerate(lines):
+        if line.startswith(key):
+            lines[i] = new_line
+            break
+    else:
+        lines.append(new_line)
+    write_env(env_path=env_path, new_data=lines)
+    return lines
+
+
+def cmd_set_telegram_token(args: Namespace):
+    set_env_value(env_path='.env', key=args.key, line=args.value)
+
+
+def cmd_get_telegram_token(args: Namespace):
+    print(get_env_value(env_path='.env', key=args.key))
 
 
 def cli():
-    subparser = parser.add_subparsers(required=True)
-    p_get = subparser.add_parser("get_token", help="Get info Telegram bot token")
-    p_get.set_defaults(func=get_telegram_token)
-    p_set = subparser.add_parser("set_token", help="Set Telegram bot token")
-    p_set.add_argument("token", help="Telegram token")
-    p_set.set_defaults(func=set_telegram_token)
+    parser = ArgumentParser()
+    sub_parser = parser.add_subparsers(required=True)
+    p_get = sub_parser.add_parser("get", help="Get new value")
+    p_get.add_argument("key", help="TELEGRAM_BOT_TOKEN or TELEGRAM_USER_ID")
+    p_get.set_defaults(func=cmd_get_telegram_token)
+    p_set = sub_parser.add_parser("set", help="Set new value")
+    p_set.add_argument("key", help="TELEGRAM_BOT_TOKEN or TELEGRAM_USER_ID")
+    p_set.add_argument("value", help="New value for TELEGRAM_BOT_TOKEN or TELEGRAM_USER_ID")
+    p_set.set_defaults(func=cmd_set_telegram_token)
     args = parser.parse_args()
     args.func(args)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     cli()
