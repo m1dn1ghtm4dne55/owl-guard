fc0a68433f5d3a983bfe813faec2e56e7fc9c55d
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2025-12-29 22:53:03 +0300

    Убран лишний код, сделано более универсальным

--- src/cli.py
+++ src/cli.py
@@ -1,25 +1,24 @@
 from argparse import ArgumentParser, Namespace
+
 from services.env_service import env_service
 
 
-def handle_env(args: Namespace) -> None:
+def handle_env(args: Namespace):
     if args.command == "get":
         print(env_service.get_env_value(key=args.key))
     elif args.command == "set":
         env_service.set_env_value(key=args.key, line=args.value)
 
 
-def cli() -> None:
+def cli():
     parser = ArgumentParser()
     sub_parser = parser.add_subparsers(required=True, dest="command")
-
     for cmd in ("get", "set"):
         cmd_line_parser = sub_parser.add_parser(cmd, help=f"{cmd} env value")
         cmd_line_parser.add_argument("key", help="TELEGRAM_BOT_TOKEN or TELEGRAM_USER_ID")
         if cmd == "set":
             cmd_line_parser.add_argument("value", help="New value")
         cmd_line_parser.set_defaults(func=handle_env)
-
     args = parser.parse_args()
     args.func(args)
 
