d0af09450010870ebce2a0d6dc350342f897746d
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2026-01-03 18:17:03 +0300

    исправлено смысловое задвоение текста

--- src/cli.py
+++ src/cli.py
@@ -18,7 +18,7 @@ def cli():
         cmd_line_parser = sub_parser.add_parser(cmd, help=f"{cmd} env value")
         cmd_line_parser.add_argument("key", help=f"{env_service.get_env_keys()}")
         if cmd == "set":
-            cmd_line_parser.add_argument("value", help="New value\nfor LOG_FILE_MAX_BYTE and TELEGRAM_USER_ID value mast be integer")
+            cmd_line_parser.add_argument("value", help="For LOG_FILE_MAX_BYTE and TELEGRAM_USER_ID value mast be integer")
         cmd_line_parser.set_defaults(func=handle_env)
     args = parser.parse_args()
     args.func(args)
