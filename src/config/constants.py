ecfa50186b0f185d458aba3bd9d4511a10c80904
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2026-01-04 22:05:57 +0300

    добавлена константа информационная для помошника CLI

--- src/config/constants.py
+++ src/config/constants.py
@@ -12,3 +12,4 @@ LOG_FILE_PATH = environ.get('LOG_FILE_PATH', '/var/log/owl-guard.log')
 WEBHOOK_URL = environ.get('WEBHOOK_URL', getenv('WEBHOOK_BOT_URL'))
 LOG_FILE_MAX_BYTES = int(environ.get('LOG_FILE_MAX_BYTES', 524288000))
 DBUS_CORE_SESSION_TIMEOUT = float(environ.get('DBUS_CORE_SESSION_TIMEOUT', 2.0))
+HELP_INFO = 'LOG_FILE_MAX_BYTE is integer | TELEGRAM_USER_ID is integer | TELEGRAM_BOT_TOKEN is token | WEBHOOK_URL is url | LOG_FILE_PATH is path | DBUS_CORE_SESSION_TIMEOUT is float or integer'
