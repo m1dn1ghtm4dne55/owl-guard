b9cd122ed53f88fd01bc5c9ad32f40d338ce621c
m1dn1ghtm4dne55 <a.parezhev@outlook.com>
2026-01-06 00:31:29 +0300

    изменение типизации

--- src/cli.py
+++ src/cli.py
@@ -5,7 +5,7 @@ from services.env_service import env_service
 from utils.cli_validator import CLIValidator
 
 
-def handle_env(args: Namespace):
+def handle_env(args: Namespace) -> None:
     key = args.key.upper()
     cli_validator = CLIValidator(args)
     value = cli_validator.validate
@@ -18,7 +18,7 @@ def handle_env(args: Namespace):
                 print('To make changes you need to restart the owl-guard.service')
 
 
-def cli():
+def cli() -> None:
     parser = ArgumentParser()
     sub_parser = parser.add_subparsers(required=True, dest="command")
     for cmd in ("get", "set"):
